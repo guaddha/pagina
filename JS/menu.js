e7762035059d998985bf9bf5c77e891ff8d83909
lourdesquintana <lulyarismendi@gmail.com>
2022-12-12 17:54:11 -0300

    agregando el toggle a la navmenu con js

--- JS/menu.js
+++ JS/menu.js
@@ -3,5 +3,5 @@ const navMenu = document.querySelector('.nav__list');
 
 iconoMenu.addEventListener('click', ()=>{
     // navMenu.style.transform = 'translateX(0)';
-   
+    navMenu.classList.toggle('nav__list--show');
 });
